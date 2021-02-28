5010105b94f762aa7550478dbc336acb43e66e57
suryo <suryoatm@gmail.com>
2021-02-28 13:21:43 +0700

    Struktur Data PHP

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PHP-Struktur_Data
+# PHP Struktur Data
+Berikut adalah source code model struktur data menggunakan bahasa pemrograman PHP
+
+# Cara Menjalankan
+
+* Install webserver seperti apache atau nginx pada local, alternatif bisa menggunakan [xampp](https://www.apachefriends.org/xampp-files/7.3.27/xampp-windows-x64-7.3.27-0-VC15-installer.exe) 
+
+* buat folrder struktur_data_php dan copy repository pada htdocs
---
+++ stack.php
@@ -0,0 +1,66 @@
+<?php
+
+class Stack {
+
+    protected $stack;
+    protected $limit;
+
+    public function __construct($limit = 10, $initial = array()) {
+        // initialize the stack
+        $this->stack = $initial;
+        // stack can only contain this many items
+        $this->limit = $limit;
+    }
+
+    public function push($item) {
+        // trap for stack overflow
+        if (count($this->stack) < $this->limit) {
+            // prepend item to the start of the array
+            array_unshift($this->stack, $item);
+        } else {
+            throw new RunTimeException('Stack is full!');
+        }
+    }
+
+    public function pop() {
+        if ($this->isEmpty()) {
+            // trap for stack underflow
+            throw new RunTimeException('Stack is empty!');
+        } else {
+            // pop item from the start of the array
+            return array_shift($this->stack);
+        }
+    }
+
+    public function top() {
+        return current($this->stack);
+    }
+
+    public function isEmpty() {
+        return empty($this->stack);
+    }
+
+}
+
+/**
+ * This'll work as expected.
+ */
+$stack = new Stack();
+$stack->push(1);
+$stack->push(2);
+$stack->push(3);
+$stack->push(4);
+$stack->push(5);
+
+
+
+
+/**
+ * And this too.
+ */
+$stack = new Stack(10, array(1, 2, 3, 4, 5));
+
+
+print_r($stack)
+
+?>
