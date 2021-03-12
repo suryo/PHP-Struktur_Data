f167e98ba23ad3ab091e959ced47a62b04ebb225
suryo <suryoatm@gmail.com>
2021-03-12 21:17:26 +0700

    pengenalan PHP

--- index.php
+++ index.php
@@ -7,6 +7,41 @@ echo "Kadang komputernya sampai hang dan saya bingung mau ngapain";
 $txt = "Hello world!";
 $x = 5;
 $y = 10.5;
-//tanpilkan isi variable
+$namadepan = "prio";
+$namatengah = "dwi";
+$namabelakang = "santoso";
+//tampilkan isi variable
 echo "<br><br><br><br><br>".$txt;
+echo "<br>faisal "."rahma";
+echo "<br>ini isi dari variable x: ".$x;
+echo "<br>";
+echo $namadepan." ".$namatengah." ".$namabelakang;
+
+//mencoba kondisi
+echo "<br>";
+$t = 20;
+echo "<br>";
+echo "isi dari variable T : ".$t;
+if ($t > "20") {
+echo "<br>Have a good day!";
+}
+elseif ($t == "20")
+{
+echo "<br>isi dari variable nya sama dengan 20";  
+}
+else
+{
+echo "<br>nilainya bedo cak!!";
+}
+//belajar perulangan
+echo "<br>";
+for ($x = 5; $x <= 100; $x++) {
+    echo "The number is: $x <br>";
+  } 
+
+//tugas wajib atau ndak wajib
+ $text = "ana febri yanti";
+ echo " itnay irbef ana"; 
+
+
 ?>
