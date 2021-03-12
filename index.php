57946c00ef22f6af1f8b0b5ea02cfd7924154d9b
suryo <suryoatm@gmail.com>
2021-03-12 20:29:15 +0700

    perkenalan bahasa php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "hello world";
+echo "<h1>About me</h1>";
+echo "Saya adalah programmer PHP yang suka PHPin komputer<br>";
+echo "Kadang komputernya sampai hang dan saya bingung mau ngapain";
+//membuat variable dengan menggunakan bahasa PHP
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+//tanpilkan isi variable
+echo "<br><br><br><br><br>".$txt;
+?>
