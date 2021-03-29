783cde213da443c54c15c41b1eec67fd1e3e0143
suryo atmojo <suryoatm@gmail.com>
2021-03-29 20:31:46 +0700

    modif stack file

--- stack1.php
+++ stack1.php
@@ -1,4 +1,19 @@
 <?php
+/* 
+*coba deh habiskan waktu kalian buat belajar coding-coding di bawah ini 4 tahun ajah.
+*lupakan sementara urusan asmara, fokus kan sejenak dengan tujuan belajar
+*bodo amat sama pacar yang sok mengekang kebebasan bercoding
+*sok nangis minta perhatian, padahal tau kita ini masih berjuang dengan impian
+*mereka bukan pasangan hidup yang sudah sah, kenapa kita harus susah susah
+*please..... tahan dulu urusan percintaan
+*tunggu kalian benar benar siap
+*dan...ketika masa itu telah tiba, ketika 4 tahun telah terlewati
+*gelar sarjana dan pekerjaan yang mapan sudah diraih
+*saat nya kalian bilang ke calon mertua...
+*titik koma aja kuperhatikan, apalagi anak bapak..
+*==sya terima nikahnya dia binti anu dengan mas kawin saham microsoft 51%....sahhhhhh*
+ */
+
 //deklarasi batasan array
 $limit = 2;
 function pembukaan(){
@@ -12,7 +27,7 @@ function pop(){
   }
 
 function push($array = array(),$isi,$limit){
-    //echo "ini method push-untuk memasukkan data";
+    //ini method push-untuk memasukkan data
     if (count($array) <= $limit)
     {
         array_push($array,$isi);
@@ -28,7 +43,7 @@ function push($array = array(),$isi,$limit){
   }
 
 function peek($array = array(), $limit){
-    //echo "ini method peek-untuk melihat data paling atas";
+    //ini method peek-untuk melihat data paling atas
     if (count($array) < $limit)
     {
         $index_atas = count($array);
