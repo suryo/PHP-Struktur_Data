072825c8b57de2a23d5ca4d01ca8e56a7e3da247
suryo <suryoatm@gmail.com>
2021-06-17 21:33:59 +0700

    search

--- sequential.php
+++ sequential.php
@@ -1,5 +1,5 @@
 <?php
-$a = array(73, 29, 49, 70, 17);
+$a = array(73, 29, 49, 70, 17, 5, 10, 20, 85);
 
 echo "isi dari count a adalah ".count($a);
 echo "<br>";
@@ -9,31 +9,38 @@ for($i = 0; $i < count($a); $i++)
        echo "<br>";
     }
 
- for($d = 0; $d <= 10; $d=$d+2)
- {
-     echo "isi dari d adalah = ".$d;
-     echo "<br>";
- }   
+//  for($d = 0; $d <= 10; $d=$d+2)
+//  {
+//      echo "isi dari d adalah = ".$d;
+//      echo "<br>";
+//  }   
 
 echo "ini sequential search";
 echo "<br>";
 
 //misal saya mencari angka 49 dari array a
-
-// for($i = 0; $i < count($a); $i++)  
-//     { 
-//         if ($a[$i] == 49)
-//         {
-
-//         }
-//         else
-//         {
-
-//         }
-    //    echo "isi array a ke ".$i." : ".$a[$i]; 
-    //    echo "<br>";
-    // }
-
+$cari = 17;
+echo "angka yang anda cari adalah ".$cari;
+echo "<br>";
+$kucing = "meong";
+for($i = 0; $i < count($a); $i++)  
+    { 
+        $kucing = "takut guk guk";
+        if ($a[$i] == $cari)
+        {
+        echo $a[$i]." ";
+        echo "ketemu";
+        echo "<br>";
+        break;
+        }
+        else
+        {
+        echo $a[$i]." ";
+        echo "tidak ketemu";
+        echo "<br>";
+        }
+    }
+//echo $kucing;
 
 
 
